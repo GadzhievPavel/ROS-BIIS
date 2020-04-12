a895af2f04825386ba7dd565b1c09476b37a2658
GadzhievPavel <gadzhievpasha@gmail.com>
2020-04-13 02:55:59 +0300

    LR4_scan

--- src/LR4/src/LR4_process_scan_node.cpp
+++ src/LR4/src/LR4_process_scan_node.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
+#include <vector>
 static sensor_msgs::LaserScan scan;
 bool flag = false;
 void callbackScan(const sensor_msgs::LaserScan &data){
@@ -16,8 +17,18 @@ int main(int argc, char **argv)
   ros::Rate r(10);
   while (ros::ok()) {
     if(flag){
-      for (;;) {
+      std::vector <float> crit;
+      for (int i = 1;i< scan.ranges.size();i++) {
+        float preRange, range, postRange;
+        try {
+          range = scan.ranges.at(i);
+          preRange = scan.ranges.at(i-1);
+          postRange = scan.ranges.at(i+1);
 
+          crit.push_back(abs(abs(range - preRange)+abs(postRange - range) - 2*abs(postRange - preRange)));
+        } catch (std::out_of_range) {
+          ROS_INFO("WARN %d out of range",i);
+        }
       }
       flag = false;
     }
