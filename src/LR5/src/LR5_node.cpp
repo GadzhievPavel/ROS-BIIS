ab55c9e62c6b59a7a4619a0f8c84b2c2696acd6b
GadzhievPavel <gadzhievpasha@gmail.com>
2020-04-25 00:13:51 +0300

    LR5

--- src/LR5/src/LR5_node.cpp
+++ src/LR5/src/LR5_node.cpp
@@ -7,8 +7,6 @@
 static sensor_msgs::LaserScan scan;
 static nav_msgs::Odometry odom;
 static ros::Time current_time;
-
-static sensor_msgs::PointCloud pointCloud;
 static bool flag = false;
 
 void callbackLaser(const sensor_msgs::LaserScan &data){
@@ -33,13 +31,11 @@ int main(int argc, char **argv)
   ros::Subscriber subOdom = nh.subscribe("/odom",1000,callbackOdom);
   tf::StampedTransform transform;
   tf::TransformBroadcaster br;
-  ros::Publisher pub = nh.advertise<sensor_msgs::PointCloud>("/point_cloud_tf",1000);
+ // ros::Publisher pub = nh.advertise<sensor_msgs::PointCloud>("/point_cloud_tf",1000);
   ros::Rate r(10.0);
-  pointCloud.header.frame_id="odom";
   while(ros::ok()){
     if (flag){
-      pointCloud.points.clear();
-      pointCloud.channels.clear();
+
       tf::Quaternion q(odom.pose.pose.orientation.x,
                        odom.pose.pose.orientation.y,
                        odom.pose.pose.orientation.z,
@@ -53,36 +49,9 @@ int main(int argc, char **argv)
       transform.frame_id_ ="/base_link";
       transform.child_frame_id_="/odom";
 
-      sensor_msgs::ChannelFloat32 channel;
-      for (int i=0;i < scan.ranges.size();i++) {
-        geometry_msgs::Point32 point;
-        point.x = scan.ranges.at(i)*sin(i*scan.angle_increment+scan.angle_min);
-        point.y = scan.ranges.at(i)*cos(i*scan.angle_increment+scan.angle_min);
-        point.z = 0;
-
-        point.x += odom.pose.pose.position.x;
-        point.y += odom.pose.pose.position.y;
-        point.z += odom.pose.pose.position.z;
-
-        point.x = cos(yaw)*point.x - sin(yaw)*point.y;
-        point.y = sin(yaw)*point.x + cos(yaw)*point.y;
-
-        point.x = cos(pitch)*point.x + sin(pitch)*point.z;
-        point.z = -sin(pitch)*point.x + cos(pitch)*point.z;
-
-        point.y = cos(roll)*point.y - sin(roll)*point.z;
-        point.z = sin(roll)*point.y + cos(roll)*point.z;
-
-        ROS_INFO("POINT x%f y%f z%f",point.x,point.y,point.z);
-        pointCloud.header.stamp=current_time;
-        pointCloud.points.push_back(point);
-        channel.name = "channel 1";
-        channel.values.push_back(100);
-      }
-      pointCloud.channels.push_back(channel);
     }
     br.sendTransform(transform);
-    pub.publish(pointCloud);
+    //pub.publish(pointCloud);
     ros::spinOnce();
     r.sleep();
   }
--- src/LR5_ICP/src/LR5_ICP_node.cpp
+++ src/LR5_ICP/src/LR5_ICP_node.cpp
@@ -1,64 +1,135 @@
 #include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
 #include <sensor_msgs/PointCloud.h>
+#include <geometry_msgs/Point.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/registration/icp.h>
+#include <tf/transform_broadcaster.h>
 static sensor_msgs::PointCloud cloud;
+static sensor_msgs::PointCloud globalCloud;
+static sensor_msgs::LaserScan scan;
 static bool flag = false;
 static bool flagPCL = false;
-pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZ>);
-pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out(new pcl::PointCloud<pcl::PointXYZ>);
+static pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZ>);
+static pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out(new pcl::PointCloud<pcl::PointXYZ>);
 
-void callbackPoints(const sensor_msgs::PointCloud &data){
-  cloud = data;
+void callbackPoints(const sensor_msgs::LaserScan &data){
+  scan = data;
   flag = true;
+  cloud.points.clear();
+  cloud.channels.clear();
+  sensor_msgs::ChannelFloat32 channel;
+  channel.name = "ch1";
+  for(int i =0; i < scan.ranges.size(); i++){
+    geometry_msgs::Point32 point;
+    point.x = scan.ranges.at(i)*sin(i*scan.angle_increment+scan.angle_min);
+    point.y = scan.ranges.at(i)*cos(i*scan.angle_increment+scan.angle_min);
+    if(!(point.x == point.x) || !(point.y == point.y) || (point.x == INFINITY) || (point.x == -INFINITY)
+       || (point.y == INFINITY) || (point.y == -INFINITY)){
+      point.x=0;
+      point.y=0;
+    }
+    cloud.points.push_back(point);
+    channel.values.push_back(100);
+
+  }
+  cloud.header.frame_id="odom";
+  cloud.header.stamp = scan.header.stamp;
+  cloud.channels.push_back(channel);
+  ROS_INFO("SIZE POINT %d", cloud.points.size());
 }
 
+bool findInCloud(geometry_msgs::Point p, sensor_msgs::PointCloud cloud){
+  bool flag = true;
+  for(int i = 0; i < cloud.points.size(); i++){
+    if((p.x == cloud.points.at(i).x) || (p.y == cloud.points.at(i).y)){
+      flag = false;
+    }
+  }
+  return flag;
+}
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "LR5_ICP_node");
   ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe("/point_cloud_tf",1000,callbackPoints);
+  ros::Subscriber sub = nh.subscribe("/scan",1000,callbackPoints);
   ros::Publisher pub = nh.advertise<sensor_msgs::PointCloud>("pcl_cloud",1000);
+  tf::StampedTransform transform;
+  tf::TransformBroadcaster br;
   ros::Rate r(10);
 
+
+  globalCloud.header.frame_id = "pcl";
   while (ros::ok()) {
 
     if(flag){
-      cloud_in->width = 16;
-      cloud_in->height = 32;
+
+      globalCloud.header.stamp=cloud.header.stamp;
+      cloud_in->width = cloud.points.size();
+      cloud_in->height = 1;
       cloud_in->is_dense =false;
       cloud_in->resize(cloud_in->width * cloud_in->height);
-      ROS_INFO("RESIZE %d",cloud_in->points.size());
+      //ROS_INFO("RESIZE %d",cloud_in->points.size());
       for (size_t i = 0; i < cloud.points.size(); ++i){
         cloud_in->points.at(i).x = cloud.points.at(i).x;
         cloud_in->points.at(i).y = cloud.points.at(i).y;
         cloud_in->points.at(i).z = cloud.points.at(i).z;
-        ROS_INFO("ITERATION %d", i);
+        //ROS_INFO("ITERATION %d", i);
       }
-      if(flagPCL){
-        ROS_INFO("PCL start");
+      ROS_INFO("PCL start");
+      if(!cloud_out->empty()){
         pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
         icp.setInputSource(cloud_in);
         icp.setInputTarget(cloud_out);
         icp.setMaximumIterations(50);
         pcl::PointCloud<pcl::PointXYZ> Final;
         icp.align(Final);
+        std::cout << "has converged:" << icp.hasConverged() << " score: "
+                  << icp.getFitnessScore() << std::endl;
+        //std::cout << icp.getFinalTransformation() << std::endl;
+        Eigen::Matrix4f mat = icp.getFinalTransformation();
+        std::cout<<mat<<std::endl;
+        std::cout<<"X "<<mat(0,3)<<" Y "<<mat(1,3)<<" Z "<<mat(2,3)<<std::endl;
+        sensor_msgs::ChannelFloat32 channel;
+        channel.name = "channel 1";
+
+        for (int i = 0; i<cloud.points.size();i++){
+          geometry_msgs::Point p;
+          p.x=cloud.points.at(i).x;
+          p.y=cloud.points.at(i).y;
+          p.z=cloud.points.at(i).z;
+          if(findInCloud(p,globalCloud)){
+            channel.values.push_back(100);
+            globalCloud.channels.push_back(channel);
+            globalCloud.points.push_back(cloud.points.at(i));
+          }
+        }
+
+        for(int i =0; i<globalCloud.points.size();i++){
+          globalCloud.points.at(i).x += mat(0,3);
+          globalCloud.points.at(i).y += mat(1,3);
+          globalCloud.points.at(i).z += mat(2,3);
+          globalCloud.points.at(i).x = mat(0,0)* globalCloud.points.at(i).x + globalCloud.points.at(i).y * mat(0,1);
+          globalCloud.points.at(i).y = mat(1,0)* globalCloud.points.at(i).x + globalCloud.points.at(i).y * mat(1,1);
+        }
+
+        tf::Quaternion q(mat(0,3),mat(1,3),mat(2,3),mat(3,3));
+        double yaw, pitch, roll;
+        tf::Matrix3x3 m(q);
+        m.getRPY(roll,pitch,yaw);
+        q = q.normalize();
+        transform.setRotation(q);
+        transform.frame_id_ ="/odom";
+        transform.child_frame_id_="/pcl";
+
       }
 
       *cloud_out = *cloud_in;
       flagPCL = true;
       flag = false;
-      cloud.points.clear();
-      cloud.channels.clear();
-      for(int i=0;i<cloud_out->size();i++){
-       geometry_msgs::Point32 point;
-       point.x = cloud_out->at(i).x;
-       point.y = cloud_out->at(i).y;
-       point.z = cloud_out->at(i).z;
-       cloud.points.push_back(point);
-      }
-      pub.publish(cloud);
+      br.sendTransform(transform);
+      pub.publish(globalCloud);
     }
     ros::spinOnce();
     r.sleep();
